7ec08d074bc5c4b148df1c4d4822bfd6f9fe48a9
Sandzhi-Garia Ochirov <sandzhi.garia@gmail.com>
2023-03-19 17:23:11 -0400

    Sandzhi did lesson 1.1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import React from 'react';
 
+const todoList= [{
+  id : 1,
+  title : "Homework",
+}, 
+{
+  id : 2,
+  title : "Cooking",
+}, 
+{
+  id : 3,
+  title : "Going to store",
+}]
+ 
 function App() {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div>
+        <h1>Todo List</h1>
+    <ul>
+    {todoList.map(function (item){
+  return <li key={item.id}>{item.title}</li>
+    })}
+    </ul>
     </div>
+
   );
 }
 
